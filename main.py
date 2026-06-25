4bb4d9f855ed511b65de0df2536338f1ef563820
c10ck05 <ihyeonjae317@gmail.com>
2026-06-25 15:31:11 +0900

    backend code edit

--- main.py
+++ main.py
@@ -16,17 +16,14 @@ ALGORITHM = "HS256"
 load_dotenv()
 
 conf = ConnectionConfig(
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME"),
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD"),
+    MAIL_FROM = os.environ.get("MAIL_FROM"),
+    MAIL_PORT = int(os.environ.get("MAIL_PORT")),
+    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_STARTTLS = False,
+    MAIL_SSL_TLS = True
 )
-
 email_verification_store = {}
 
 Base.metadata.create_all(bind=engine)
