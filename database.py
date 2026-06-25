6eb7fee9c7c5aa2b1d0f7f99aece0592c2824b24
c10ck05 <ihyeonjae317@gmail.com>
2026-06-26 08:43:36 +0900

    real legend final

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATEABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
